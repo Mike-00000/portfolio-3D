a9b0d5dab61fd882bd39bf012d4ea6588524b34b
Mike <mt.mail.mt@gmail.com>
2023-11-14 14:00:03 +0200

    Commitings 1th, 2nd and 3rd part

--- src/assets/index.js
+++ src/assets/index.js
@@ -20,7 +20,11 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
-
+import python from "./tech/python.png";
+import django from "./tech/django.png";
+import elephant from "./tech/elephant.png";
+import bootstrap from "./tech/bootstrap.png";
+import flstudio from "./tech/flstudio.png";
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
@@ -59,4 +63,9 @@ export {
   carrent,
   jobit,
   tripguide,
+  python,
+  django,
+  elephant,
+  bootstrap,
+  flstudio,
 };
