d41bd52b89ec616fad28afb0b369951de41e6579
Mike <mt.mail.mt@gmail.com>
2023-11-16 12:23:13 +0200

    Commiting almost complete website. Lack some fixes

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -6,7 +6,7 @@ import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from '../Utils/motion';
 
-const ProjectCard = ({ index, name, description, descripition, tags, image, source_code_link}) => {
+const ProjectCard = ({ index, name, description, tags, image, source_code_link}) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
       <Tilt
